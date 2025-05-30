31a351f8cde855013766d6b25fd0b9daf2dbe79b
ju-lchan <juchan06@naver.com>
2025-05-30 22:02:04 +0900

    20250530 21:51 main.py edit (주요 기능 생성)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import stock_data
+import stock_data 
 import id_stock_data
 import member
 
 def main():
-    pass
+    member.signup()
+    id_stock_data.csv_create()
+    id_stock_data.csv_update(stock_data.resolve_to_ticker())
 
 if __name__ == '__main__':
     main()
