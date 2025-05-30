6b50deab90b1c8ad9729f84ebab04ea1465e324f
ju-lchan <juchan06@naver.com>
2025-05-30 21:40:03 +0900

    20250530 21:39 id_stock_data.py edit (create_csv(), csv_update() 함수 생성성)

--- id_stock_data.py
+++ id_stock_data.py
@@ -6,13 +6,24 @@ import json
 json_file = Path('user_data.json')
 
 with json_file.open('rt') as fp:
-    usernames = json.load(fp).keys()
+    usernames = json.load(fp).keys() #id.json 파일의 키값만 모은 리스트
 
-for i in usernames:
-    file_exists = os.path.isfile(f"{i}.csv") # 파일이 없으면 헤더 추가, 있으면 헤더 없이 이어서 저장
+def create_csv():
+    '''
+    userdata.json에서 id를 불러와서 'id.csv' 파일을 생성성
+    '''
 
-    with open(f"{i}.csv", mode="a", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        if not file_exists:
-            writer.writerow(["ticker", "date"])  # 헤더 작성
-        # writer.writerow([id, pw])  # 사용자 정보 저장
+    for i in usernames:
+        file_exists = os.path.isfile(f"{i}.csv") # 파일이 없으면 헤더 추가, 있으면 헤더 없이 이어서 저장
+
+        with open(f"{i}.csv", mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            if not file_exists:
+                writer.writerow(["ticker", "date"])  # 헤더 작성
+            # writer.writerow([id, pw])  # 사용자 정보 저장
+
+def csv_update(stockdata):
+    for j in usernames:
+        with open(f"{j}.csv", mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(stockdata)  # 사용자 정보 저장
