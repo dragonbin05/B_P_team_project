810512dce8416e93f8c4b6d2ef0be476748ee7ff
dragonbin05 <dragonbin05@gmail.com>
2025-05-30 19:50:53 +0900

    20250530 19:50 stock_data.py, test.py edit

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-print("Hello, world")
-print("")
-print("hh")
-print("???")
+from pathlib import Path
+import json
+
+json_file = Path('user_data.json')
+
+if json_file.exists() and json_file.stat().st_size > 0:
+    with json_file.open('r', encoding='utf-8') as f:
+        data = json.load(f)
+else:
+    data = {}
+    
+id = input("ID를 입력하시오: ")
+pw = input("Password를 입력하시오: ")
+data[id] = pw
+
+
+with json_file.open('w+t') as fp:
+    json.dump(data, fp)
